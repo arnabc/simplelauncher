240908a7adb1cec5e49c2fae2a99604c103daa30
Jigar Sable <64949957+jigar-sable@users.noreply.github.com>
2020-10-01 17:17:17 +0530

    Update AppListFragment.java

--- SimpleLauncher/src/main/java/ch/arnab/simplelauncher/AppListFragment.java
+++ SimpleLauncher/src/main/java/ch/arnab/simplelauncher/AppListFragment.java
@@ -1,29 +1,21 @@
 package ch.arnab.simplelauncher;
-
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import java.util.ArrayList;
 
-/**
- * Created by Arnab Chakraborty
- */
 public class AppListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<ArrayList<AppModel>> {
     AppListAdapter mAdapter;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         setEmptyText("No Applications");
-
         mAdapter = new AppListAdapter(getActivity());
         setListAdapter(mAdapter);
-
         // till the data is loaded display a spinner
         setListShown(false);
-
         // create the loader to load the apps list in background
         getLoaderManager().initLoader(0, null, this);
     }
@@ -36,7 +28,6 @@ public class AppListFragment extends ListFragment implements LoaderManager.Loade
     @Override
     public void onLoadFinished(Loader<ArrayList<AppModel>> loader, ArrayList<AppModel> apps) {
         mAdapter.setData(apps);
-
         if (isResumed()) {
             setListShown(true);
         } else {
